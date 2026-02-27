238e2d3d601f8e51d71614ee7575909a9102e039
MaithiliSawant11 <maithilis350@gmail.com>
2026-02-27 09:27:43 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        ETHAN Racing
+      </h1>
+    </main>
+  );
+}
