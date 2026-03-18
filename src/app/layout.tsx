6eda19267108f1e79b3c6d6ee662243af9f1e747
MaithiliSawant11 <maithilis350@gmail.com>
2026-03-18 08:02:35 +0530

    mobile responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import Navbar from "../components/Navbar";
 import { Orbitron } from "next/font/google";
 import Footer from "@/components/Footer";
 
+export const metadata = {
+  title: "Ethan Racing VCET",
+};
+
 const orbitron = Orbitron({
   subsets: ["latin"],
   weight: ["400","600","700"]
@@ -15,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={orbitron.className}>
         <Navbar/>
         {children}
